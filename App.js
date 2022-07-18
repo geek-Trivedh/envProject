448dc013ae8ad7c9f8871159bb4ec2e7747234b9
geek-Trivedh <trivedhk@geekyants.com>
2022-07-18 18:51:55 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+import {API_KEY} from '@env';
+export default App = () => {
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>{API_KEY}</Text>
+    </View>
+  );
+};
